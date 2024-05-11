66014758e73fe07057ebf997f2ffa0423eec7537
TheRedHoodGuy <trhg.xavierokpalannajiaku.mez@gmail.com>
2024-05-11 18:39:57 +0100

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Headcomp from './components/Pageheader/Headcomp.jsx'
 import Article from './components/Mainarticle/Article.jsx'
 import Imagesection from './components/Imagesection/Imagesection.jsx'
