ec5917ebe949240fa2b541dcc5f5e7c03b25e736
Ayomikun <ayoadeosun10@gmail.com>
2024-05-11 20:03:41 +0100

    added functionality to menu button

--- src/components/Pageheader/Headcomp.jsx
+++ src/components/Pageheader/Headcomp.jsx
@@ -1,23 +1,23 @@
-import Navbar from './Navbar'
-import Sidemenu from './Sidemenu'
-import Logo from '../../assets/images/logo.svg'
+import Navbar from "./Navbar";
+import Sidemenu from "./Sidemenu";
+import Logo from "../../assets/images/logo.svg";
 
 const Headcomp = () => {
-    return (
-        <>
-            <header className="flex w-full items-center mt-7 min-[1440px]:mt-[90px]">
-                <div>
-                    <img className="w-[45px]" src={Logo} alt="Logo" />
-                </div>
-                <div className='hidden'>
-                    <Navbar />
-                </div>
-                <div className='ml-auto'>
-                    <Sidemenu />
-                </div>
-            </header>
-        </>
-    );
-}
+  return (
+    <>
+      <header className="flex justify-between w-full items-center mt-7 min-[1440px]:mt-[90px]">
+        <div>
+          <img className="w-[45px]" src={Logo} alt="Logo" />
+        </div>
+        <div className="hidden lg:block">
+          <Navbar />
+        </div>
+        <div className="lg:hidden">
+          <Sidemenu />
+        </div>
+      </header>
+    </>
+  );
+};
 
-export default Headcomp
+export default Headcomp;
--- src/components/Pageheader/Sidemenu.jsx
+++ src/components/Pageheader/Sidemenu.jsx
@@ -1,28 +1,63 @@
-import X from '../../assets/images/icon-menu-close.svg'
-import Menu from '../../assets/images/icon-menu.svg'
+import { useState } from "react";
+import close from "../../assets/images/icon-menu-close.svg";
+import Menu from "../../assets/images/icon-menu.svg";
 
 const Sidemenu = () => {
-    return(
-        <>
-            <div>
-                <img className="w-10" src={Menu} alt="Menu" />
-                <section className='absolute hidden'>
-                    <div>
-                        <img src={X} alt="Close" />
-                    </div>
-                    <nav>
-                        <ul>
-                        <li><a className="text-sm" href="#" target="_blank">Home</a></li>
-                        <li><a className="text-sm" href="#" target="_blank">New</a></li>
-                        <li><a className="text-sm" href="#" target="_blank">Popular</a></li>
-                        <li><a className="text-sm" href="#" target="_blank">Trending</a></li>
-                        <li><a className="text-sm" href="#" target="_blank">Categories</a></li>
-                        </ul>
-                    </nav>
-                </section>
-            </div>
-        </>
-    );
-}
+  const [isMenu, setIsMenu] = useState(false);
 
-export default Sidemenu
+  const toggleMenu = () => setIsMenu(!isMenu);
+
+  return (
+    <>
+      <div className="">
+        <div className="absolute right-[15px] text-right w-40">
+          <button onClick={toggleMenu}>
+            <img
+              className={`w-7 ${isMenu ? `mr-[-2px]` : `mr-0`}`}
+              src={isMenu ? close : Menu}
+              alt="Menu"
+            />
+          </button>
+          <section
+            className={`h-screen bg-white_c ${isMenu ? `block` : `hidden`}`}
+          >
+            {/* <div>
+                            <img src={X} alt="Close" />
+                        </div> */}
+            <nav className="">
+              <ul>
+                <li>
+                  <a className="text-sm" href="#" target="_blank">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a className="text-sm" href="#" target="_blank">
+                    New
+                  </a>
+                </li>
+                <li>
+                  <a className="text-sm" href="#" target="_blank">
+                    Popular
+                  </a>
+                </li>
+                <li>
+                  <a className="text-sm" href="#" target="_blank">
+                    Trending
+                  </a>
+                </li>
+                <li>
+                  <a className="text-sm" href="#" target="_blank">
+                    Categories
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidemenu;
